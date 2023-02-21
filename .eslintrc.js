2962a92c5547cd748361fc63be70a3a69804a091
Ludvig <l.lundh@hotmail.com>
2023-02-21 17:43:02 +0100

    fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,15 +6,12 @@ module.exports = {
     jest: true,
   },
   parser: '@typescript-eslint/parser',
-  plugins: ['react-hooks', '@typescript-eslint', 'import'],
+  plugins: ['react-hooks', '@typescript-eslint'],
   extends: [
-    'next/core-web-vitals',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
     'plugin:react-hooks/recommended',
-    'plugin:import/warnings',
-    // 'plugin:import/typescript',
   ],
   parserOptions: {
     ecmaFeatures: {
@@ -28,15 +25,6 @@ module.exports = {
     'no-console': 'warn',
     'react/jsx-use-react': 'off',
     'react/react-in-jsx-scope': 'off',
-    'import/extension': 'off',
-    'import/no-anonymous-default-export': [
-      'error',
-      {
-        allowAnonymousFunction: true,
-      },
-    ],
-    'import/no-named-as-default-member': 'off',
-    'import/no-named-as-default': 'off',
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
   },
